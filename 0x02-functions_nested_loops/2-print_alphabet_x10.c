f80415fad0247f923878a3eff0c6b4bf3427002b
Dc-dike <daviddike247@gmail.com>
2023-03-22 04:37:35 -0700

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints alphabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char a;
+
+	for (i = 1; i <= 10 ; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar('i');
+			_putchar('\n');
+	}
+}
