7e0949fd9bc73c832447ad7515557226291896ab
Dc-dike <daviddike247@gmail.com>
2023-03-23 17:00:00 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - function checks that number is in the rage of 0 to 9
+ * @c: the number to be checked
+ * Return: 1 if the number is in the rangr of 0 to 9
+ * and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
+
